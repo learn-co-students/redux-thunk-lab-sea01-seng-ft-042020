a79a76879049c82834c76acfa1c67b49c0d781c7
github username <github email address>
2020-08-13 13:39:02 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import catsReducer from "./reducers/catsReducer";
 
-const store = createStore(catsReducer, applyMiddleware(thunk));
+const store = createStore(
+  catsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
